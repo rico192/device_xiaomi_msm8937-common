3e1b4cc6d67b89457fcb025f746299f7b6df4231
Akmal <magchuz@pm.me>
2020-01-28 10:29:10 +0000

    msm8937-common: Kill QC Location SDK

--- msm8937.mk
+++ msm8937.mk
@@ -195,9 +195,6 @@ PRODUCT_PACKAGES += \
     libgnsspps \
     libcurl
 
-PRODUCT_BOOT_JARS += \
-    com.qti.location.sdk
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
